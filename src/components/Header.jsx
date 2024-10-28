ab4d2bcbccae89649d4470e020abf3d58d7aa29d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-28 14:16:10 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,17 @@
+import Logo from "../assets/logo.jpg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={Logo} alt="resturant logo" />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;

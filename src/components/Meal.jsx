4749ce262fc6b447db45f38ea584d07b8ca0b4d6
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-28 16:47:13 +0300

    Update Meal.jsx

---
+++ src/components/Meal.jsx
@@ -0,0 +1,19 @@
+const Meal = ({ mealImg, name, price, description }) => {
+  return (
+    <li className="meal-item">
+      <article>
+        <img src={`http://localhost:3000/${mealImg}`} alt={name} />
+        <div>
+          <h3>{name}</h3>
+          <p className="meal-item-price">${price}</p>
+          <p className="meal-item-description">{description}</p>
+        </div>
+        <p className="meal-meal-item-actions">
+          <button>Add to cart</button>
+        </p>
+      </article>
+    </li>
+  );
+};
+
+export default Meal;

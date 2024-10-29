4f899fac4a8502c6b3f88d7f06ab878cf9627728
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-30 02:11:37 +0300

    Update Modal.jsx

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,5 +1,23 @@
-const Modal = () => {
-  return <div>Modal</div>;
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children, open }) => {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
+
+  return createPortal(
+    <dialog ref={dialog} className="modal">
+      {children}
+    </dialog>,
+    document.getElementById("modal")
+  );
 };
 
 export default Modal;

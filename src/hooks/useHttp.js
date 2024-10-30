dab1ebc4e7a46512c60d4dbd54133858643271c3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-31 01:04:50 +0300

    Update useHttp.js

--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -1,10 +1,37 @@
+import { useState } from "react";
+
 const sendHttpRequest = async (url, config) => {
   const response = await fetch(url, config);
+  const data = await response.json();
 
   if (!response.ok) {
+    throw new Error(data.message || "failed to send request");
   }
 
-  const data = await response.json();
+  return data;
 };
 
-export default function useHttp() {}
+export default function useHttp() {
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState();
+
+  const sendRequest = async () => {
+    setLoading(true);
+
+    try {
+      const resData = await sendHttpRequest();
+      setData(resData);
+    } catch (error) {
+      setError(error.message || "failed to send request!");
+    }
+
+    setLoading(false);
+  };
+
+  return {
+    data,
+    error,
+    loading,
+  };
+}

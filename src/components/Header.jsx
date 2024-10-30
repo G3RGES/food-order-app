fe7ea1c56e5cbaec3bf3f6fe06a4fc75871c6cd4
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-30 04:06:46 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,18 @@ import { useContext } from "react";
 import Logo from "../assets/logo.jpg";
 import Button from "./UI/Button";
 import CartContext from "../store/CartContext";
+import Modalcontext from "../store/ModalContext";
 
 const Header = () => {
+  const { showCart } = useContext(Modalcontext);
   const { items } = useContext(CartContext);
   const totalCartItems = items.reduce((total, item) => {
     return total + item.quantity;
   }, 0);
+
+  const showCartModal = () => {
+    showCart();
+  };
 
   return (
     <header id="main-header">
@@ -16,7 +22,9 @@ const Header = () => {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button textOnly onClick={showCartModal}>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );

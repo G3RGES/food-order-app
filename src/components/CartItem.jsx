4389bc8ad42268faf2b53980e3de3145d21b883c
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-30 04:40:26 +0300

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,18 @@
-const CartItem = () => {
-  return <li className="cart-item">CartItem</li>;
+import { currencyFormatter } from "../util/formatting";
+
+const CartItem = ({ name, quantity, price }) => {
+  return (
+    <li className="cart-item">
+      <p>
+        {name}-{quantity} X {currencyFormatter.format(price)}
+      </p>
+      <p className="cart-item-actions">
+        <button>-</button>
+        <span>{quantity}</span>
+        <button>+</button>
+      </p>
+    </li>
+  );
 };
 
 export default CartItem;

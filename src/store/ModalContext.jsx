22c63aa4feb8e5803e1c66d5befe5dbb82eee223
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-30 03:13:16 +0300

    Update ModalContext.jsx

--- src/store/ModalContext.jsx
+++ src/store/ModalContext.jsx
@@ -1,5 +1,43 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useState } from "react";
 
-const Modalcontext = {};
+const Modalcontext = createContext({
+  progress: "",
+  showCart: () => {},
+  hideCart: () => {},
+  showCheckout: () => {},
+  hideCheckout: () => {},
+});
 
-export function ModalContextProvider({ children }) {}
+export function ModalContextProvider({ children }) {
+  const [userProgress, setUserProgress] = useState("");
+
+  function showCart() {
+    setUserProgress("cart");
+  }
+
+  function hideCart() {
+    setUserProgress("");
+  }
+
+  function showCheckout() {
+    setUserProgress("checkout");
+  }
+
+  function hideCheckout() {
+    setUserProgress("");
+  }
+
+  const modalCtx = {
+    progress: userProgress,
+    showCart,
+    hideCart,
+    showCheckout,
+    hideCheckout,
+  };
+
+  return (
+    <Modalcontext.Provider value={modalCtx}>{children}</Modalcontext.Provider>
+  );
+}
+
+export default Modalcontext;

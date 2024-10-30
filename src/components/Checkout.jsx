7b666ec91f9a38c0bcb928112b907e3dc88034a3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-30 05:17:48 +0300

    Update Checkout.jsx

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,5 +1,22 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../util/formatting";
+
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const { items } = useContext(CartContext);
+  const cartTotal = items.reduce((totalPrice, item) => {
+    return totalPrice + item.price * item.quantity;
+  }, 0);
+
+  return (
+    <Modal>
+      <form className="">
+        <h2>Checkout</h2>
+        <p>Total Amount: {currencyFormatter.format(cartTotal)} </p>
+      </form>
+    </Modal>
+  );
 };
 
 export default Checkout;
